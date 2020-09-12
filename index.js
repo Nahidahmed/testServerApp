b86065f17ea41d9aaf7b7167fff6737cb6c312ef
Nahid <nahid.mansuri@gmail.com>
2020-09-13 00:32:27 +0530

    dynamically render web page from server

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine','ejs');
+
+
+var logger = (req,res,next) =>{
+    var url = req.url;
+    var time = new Date();
+    console.log(`Received request for, ${url} at ${time}`);
+    next();
+};
+
+//if you want to call logger for each route
+//app.use(logger);
+
+
+//if you want to call logger(), include logger() as param and observer the logs on the Terminal from where
+// the js app was launched
+app.use('/public',express.static('files'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/handleForm',(req,res) =>{
+    var name = req.body.username;
+    var hobbies = [].concat(req.body.hobby);
+    res.render('showHobbies',{
+        name: name,
+        hobbies: hobbies
+    });
+});
+
 app.use(
     '/index',(req,res) => {
 
@@ -24,8 +54,6 @@ app.get('/about',function(res,res){
     }
 );
 
-
-app.use('/public',express.static('files'));
 
 app.listen(
     3000, () =>{
